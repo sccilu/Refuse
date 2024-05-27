f9a11114bffa74adf9d492b4fe3ba4ca367aaf99
86180 <2428129@qq.com>
2024-05-27 10:10:15 +0800

    增加反馈功能，用户可以通过这个功能实现意见反馈

--- app/src/main/java/com/example/refuseclassification/guidefragment/DryFragment.java
+++ app/src/main/java/com/example/refuseclassification/guidefragment/DryFragment.java
@@ -1,6 +1,6 @@
 package com.example.refuseclassification.guidefragment;
 
-import  android.os.Bundle;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
