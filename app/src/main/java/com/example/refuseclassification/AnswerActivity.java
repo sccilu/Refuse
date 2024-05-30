cc80e03e8d37d9917305965b32998bc6bcb16bf8
sccilu <93533134+sccilu@users.noreply.github.com>
2024-05-31 00:09:29 +0800

    KnowledgeDatabase.java添加数据库记录 添加测试类

--- app/src/main/java/com/example/refuseclassification/AnswerActivity.java
+++ app/src/main/java/com/example/refuseclassification/AnswerActivity.java
@@ -1,7 +1,6 @@
 package com.example.refuseclassification;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
